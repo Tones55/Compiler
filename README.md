3fdbf2ea2e729c991b3f6632520a03022d20d00c
Tones55 <54862709+Tones55@users.noreply.github.com>
2023-01-29 12:30:46 -0500

    using java, add Compiler.java

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Compiler
 A Compiler built for Design of Compilers class
+
+Compiler.java is the entry point
---
+++ src/Compiler.java
@@ -0,0 +1,6 @@
+package src;
+public class Compiler {
+    public static void main(String[] args) {
+        
+    }
+}
