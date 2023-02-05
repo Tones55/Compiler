840c91224d889c1615d0ded43899dfa049dfd1e9
Tones55 <54862709+Tones55@users.noreply.github.com>
2023-02-04 20:45:41 -0500

    position changes applied to token class

--- src/Token.java
+++ src/Token.java
@@ -4,29 +4,25 @@ public class Token {
 
     private String name; //keyword,identifier,symbol,digit,character
     private String value; //the actual value of the token
-    private int line;
-    private int column;
+    private Position position;
 
-    public Token(String name, String value, int line, int column) {
+    public Token(String name, String value, Position position) {
         this.name = name;
         this.value = value;
-        this.line = line;
-        this.column = column;
+        this.position = position;
     }
 
     //getters
     public String getName() { return name; }
     public String getValue() { return value; }
-    public int getLine() { return line; }
-    public int getColumn() { return column; }
+    public Position getPosition() { return position; }
 
     //setters
     public void setName(String name) { this.name = name; }
     public void setValue(String value) { this.value = value; }
-    public void setLine(int line) { this.line = line; }
-    public void setColumn(int column) { this.column = column; }
+    public void setPosition(Position position) { this.position = position; }
 
     public String toString() {
-        return "Found Token: " + name + " at location " + "Line: " + line + " Column: " + column;
+        return "Found Token: " + name + " at location " + position.toString();
     }
 }
