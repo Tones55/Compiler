acac5c8b6c2f8b1d41fe8b9ef1e8155a7d3084dd
Tones55 <54862709+Tones55@users.noreply.github.com>
2023-02-06 23:05:30 -0500

    start lexing process

--- src/Lexer.java
+++ src/Lexer.java
@@ -4,34 +4,174 @@ import java.util.ArrayList;
 
 public class Lexer {
 
-    private boolean verbose = true;
-    private ArrayList<String> program;
-    private boolean isString = false;
-    private static ArrayList<Token> tokens = new ArrayList<Token>();
-    private static Token lastToken = null;
-    private Position position;
-    private Position boundry = null;
+    private static boolean verbose = true;
+    private static ArrayList<String> program;
+    private static ArrayList<Token> tokens;
+    private static Token lastToken;
+    private static Token currentToken;
+    private static Position lastPosition;
+    private static Position currentPosition;
+    private static boolean isString;
 
-    public Lexer(ArrayList<String> program){
-        this.program = program;
+    public static ArrayList<Token> doLex(ArrayList<String> prog){
+        program = prog;
 
-        position = new Position(1,1);
-        for(int i=0; i<program.size(); i++) {
-            lex(program.get(i));
-        }
+        tokens = new ArrayList<Token>();
+        lastToken = null;
+        currentToken = null;
+        lastPosition = new Position(1,1);
+        currentPosition = new Position(1,1);
+        isString = false;
+
+        lex(program.get(0));
+        return tokens;
     }
 
-    private void lex(String currentWork){
+    private static void lex(String currentWork){
         if(verbose) System.out.println(currentWork);
         
-        String visible = "";
+        String visible = ""; // the part of current work the lexer is currently looking at
+        String match = ""; // the type of match found for the visible string
+        String currentChar = ""; // the current character being looked at
+        String currentMatch = ""; // the type of match found for the current character
+
         for(int i=0; i<currentWork.length(); i++){
+            currentChar = currentWork.charAt(i) + "";
             visible += currentWork.charAt(i);
-            PatternMatcher.match(visible, position);
+
+            // if a quote was found lex for a string
+            if(!isString){
+                match = PatternMatcher.match(visible, lastPosition , 1);
+                currentMatch = PatternMatcher.match(currentChar , currentPosition , 2);
+
+                //if there is 1 char in visible check for 1 char matches
+                if (visible.length() == 1) {
+                    // if the current character is a boundry
+                    if (match.equals("Boundry")) {
+                        // if the single char is a boundry, skip it
+                        visible = "";
+                        currentWork = currentWork.substring(i + 1);
+                        i = -1;
+                        continue;
+                    }
+                    else if (match.equals("Quote")) {
+                        // if the single char is a quote, start lexing for a string
+                        isString = true;
+                        visible = "";
+                        currentWork = currentWork.substring(i + 1);
+                        i = -1;
+                        continue;
+                    }
+                    else if (match.equals("Digit")) {
+                        // if the single char is a digit, add it to the token list
+                        if(lastToken != null){
+                            /*tokens.add(lastToken); in clean up*/
+                            break;
+                        }
+                    }
+                    else if (match.equals("Symbol")) {
+                        // if the single char is a symbol, add it to the token list
+                        if(lastToken != null){
+                            /*tokens.add(lastToken); in clean up*/
+                            break;
+                        }
+                    }
+                    else if (match.equals("Explicit Identifier")) {
+                        // if the single char is a explicit identifier, add it to the token list
+                        if(lastToken != null){
+                            /*tokens.add(lastToken); in clean up*/
+                            break;
+                        }
+                    }
+                }
+                else {
+                    if (currentMatch.equals("Boundry") | currentMatch.equals("Quote") ) {
+                        // if a token was found add the token and go to "Clean up"
+                        if(lastToken != null){
+                            /*tokens.add(lastToken); in clean up*/
+                            break;
+                        }
+                        // if no token was found and a boundry was hit, report error
+                        else {
+                            System.out.println("Lex Error: Unknown token on line: " + lastPosition.getLine() +
+                                "near column: " + lastPosition.getColumn());
+                        }
+                    }
+                    if (currentMatch.equals("")) {
+
+                    }
+                }
+            }
+            // if in a string do this to lex instead
+            // do not exit loop for duration of string, this improves preformace 
+            else {
+                currentMatch = PatternMatcher.checkForCharList(currentChar, currentPosition);
+                // if the string contains a legal character
+                if (currentMatch.equals("Character")) {
+                    tokens.add(lastToken);
+                }
+                // if end quote was found
+                else if (currentMatch.equals("Quote")) {
+                    tokens.add(lastToken);
+                    isString = false;
+                    i = 0;
+                    currentWork = currentWork.substring(i + 1);
+                }
+                else {
+                    System.out.println("Lex Error: Cannot use character " + currentChar + " inside of a string " + " found at " + currentPosition.toString());
+                    System.exit(0);
+                }
+            }
+        }
+
+        // "Clean up" (120-147)
+        // prepares for the next string to be lexed, throws and error, or proceeds to Parse
+
+        // if a token was found shorten the working string and add the token to the list
+        if (lastToken != null) {
+            currentWork = currentWork.substring(lastToken.getValue().length());
+            tokens.add(lastToken);
+        }
+
+        // if the string is not empty
+        if(currentWork.length() > 0){
+            // if there was a token found
+            if (lastToken != null) {
+                lastToken = null;
+                lex(currentWork);
+                return;
+            }
+            // if there was no token found but there is still a string to lex
+            else {
+                System.out.println("Lex Error: Unknown token on line: " + lastPosition.getLine() +
+                "near column: " + lastPosition.getColumn());
+                System.exit(0);
+            }
+        }
+        // if the string is empty and there is a new line to lex
+        else if (currentPosition.getLine() < program.size()) {
+            // start lexing the next line
+            incrementLine();
+            currentWork = program.get(currentPosition.getLine());
+            lex(currentWork);
+            return;
+        }
+        else { return; } // if the string is empty and there is no new line to lex
+    }
+
+    public static void updateToken(String type, String value, Position position , int t){
+        if (t == 1) {
+            lastToken = new Token(type, value, position);
+        }
+        else {
+            currentToken = new Token(type, value, position);
         }
     }
 
-    public static void createtoken(String type, String value, Position position){
-        lastToken = new Token(type, value, position);
+    public static void incrementLine() {
+        lastPosition.setLine(lastPosition.getLine() + 1); 
+        lastPosition.setColumn(1);
+        currentPosition.setLine(currentPosition.getLine() + 1);
+        currentPosition.setColumn(1);
     }
 }
