39d1bd753fdb4de7a6ad898cbb1cba43be5f44b2
Tones55 <54862709+Tones55@users.noreply.github.com>
2023-02-10 13:28:05 -0500

    add patterns, update matching methods

--- src/PatternMatcher.java
+++ src/PatternMatcher.java
@@ -5,80 +5,107 @@ import java.util.regex.Matcher;
 
 public class PatternMatcher {
 
-    private static Pattern explicitIdentifier = Pattern.compile("acdeghjklmnopqruvxyz"); //used to match identifiers that are not keywords
+    private static Pattern explicitIdentifier = Pattern.compile("acdeghjklmnopqruvxyz"); // used to match identifiers that can not be keywords
     private static Pattern keyword = Pattern.compile("print|while|if|int|string|boolean|true|false");
     private static Pattern identifier = Pattern.compile("[a-z]");
     private static Pattern symbol = Pattern.compile("\\{|\\}|\\(|\\)|==|!=|\\+");
+    private static Pattern assign = Pattern.compile("=");
     private static Pattern digit = Pattern.compile("[0-9]");
-    private static Pattern character = Pattern.compile("[a-z]|\\s");
+    private static Pattern character = Pattern.compile("[a-z]|\\s"); // can only be found in quotes
     private static Pattern quote = Pattern.compile("\"");
 
-    //used to aid in the creation of tokens but are not tokens themselves
-    private static Pattern boundry = Pattern.compile("☺"); //used to match whitespace
-    private static Pattern endOfProgram = Pattern.compile("$");
+    // used to aid in the creation of tokens but are not tokens themselves
+    private static Pattern boundry = Pattern.compile("\\s|\\t|\\r"); // used to match whitespace
+    private static Pattern endOfProgram = Pattern.compile("\\$");
+    private static Pattern illegalCharacters = Pattern.compile("[^a-z0-9\\s\\t\\r\\{\\}\\(\\)\\=\\!\\+\"\\$]");
 
     public static String match(String input , Position pos , int t){
         Matcher m;
         m = explicitIdentifier.matcher(input);
-        if(m.find()){
-            Lexer.updateToken("Identifier" , input , pos , t);
-            return "Explicit Identifier";
+        String matchName = "No Match";
+        // the for loop and continue is used so all the ifs do not need to run
+        // it never loops so continue sends it to the return statement
+        for(int i=0;i<1;i++) {
+            if(m.matches()){
+                Lexer.updateToken("Identifier" , input , pos , t);
+                matchName = "Explicit Identifier";
+                continue;
+            }
+            m = keyword.matcher(input);
+            if(m.matches()){
+                Lexer.updateToken("Keyword" , input , pos , t);
+                matchName = "Keyword";
+                continue;
+            }
+            m = identifier.matcher(input);
+            if(m.matches()){
+                Lexer.updateToken("Identifier" , input , pos , t);
+                matchName = "Identifier";
+                continue;
+            }
+            m = symbol.matcher(input);
+            if(m.matches()){
+                Lexer.updateToken("Symbol" , input , pos , t);
+                matchName = "Symbol";
+                continue;
+            }
+            m = assign.matcher(input);
+            if(m.matches()){
+                Lexer.updateToken("Symbol" , input , pos , t);
+                matchName = "Assign";
+                continue;
+            }
+            m = digit.matcher(input);
+            if(m.matches()){
+                Lexer.updateToken("Digit" , input , pos , t);
+                matchName = "Digit";
+                continue;
+            }
+            m = quote.matcher(input);
+            if(m.matches()){
+                Lexer.updateToken("Quote", input, pos , t);
+                matchName = "Quote";
+                continue;
+            }
+            // not tokens but used to aid in the creation of tokens
+            m = boundry.matcher(input);
+            if(m.matches()){
+                matchName = "Boundry";
+                continue;
+            }
+            m = endOfProgram.matcher(input);
+            if(m.find()){
+                matchName = "End of Program";
+                continue;
+            }
+            m = illegalCharacters.matcher(input);
+            if(m.matches()){
+                matchName = "Illegal Character";
+                continue;
+            }
         }
-        m = keyword.matcher(input);
-        if(m.find()){
-            Lexer.updateToken("Keyword" , input , pos , t);
-            return "Keyword";
-        }
-        m = identifier.matcher(input);
-        if(m.find()){
-            Lexer.updateToken("Identifier" , input , pos , t);
-            return "Identifier";
-        }
-        m = symbol.matcher(input);
-        if(m.find()){
-            Lexer.updateToken("Symbol" , input , pos , t);
-            return "Symbol";
-        }
-        m = digit.matcher(input);
-        if(m.find()){
-            Lexer.updateToken("Digit" , input , pos , t);
-            return "Digit";
-        }
-        m = character.matcher(input);
-        if(m.find()){
-            Lexer.updateToken("Character" , input , pos , t);
-            return "Character";
-        }
-        m = quote.matcher(input);
-        if(m.find()){
-            Lexer.updateToken("Quote", input, pos , t);
-            return "Quote";
-        }
-
-        m = boundry.matcher(input);
-        if(m.find()){
-            //System.out.println("Boundry: " + input);
-            return "Boundry";
-        }
-        m = endOfProgram.matcher(input);
-        if(m.find()){
-            return "End of Program";
-        }
-        return "No Match";
+        return matchName;
     }
 
     public static String checkForCharList(String input , Position pos) {
         Matcher m = keyword.matcher(input);
         m = character.matcher(input);
-        if(m.find()){
-            Lexer.updateToken("Character" , input , pos , 1);
-            return "Character";
+        String matchName = "No Match";
+        // the for loop and continue is used so all the ifs do not need to run
+        // it never loops so continue sends it to the return statement
+        for (int i = 0; i < 1; i++) {
+            if(m.matches()){
+                Lexer.updateToken("Character" , input , pos , 1);
+                matchName = "Character";
+                continue;
+            }
+            m = quote.matcher(input);
+            if(m.matches()){
+                Lexer.updateToken("Quote", input, pos , 1);
+                matchName = "Quote";
+                continue;
+            }
         }
-        m = quote.matcher(input);
-        if(m.find()){
-            Lexer.updateToken("Quote", input, pos , 1);
-            return "Quote";
-        }
-        return "No Match";
+        return matchName;
     }
 }
