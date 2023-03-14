d58de457fd9e52e9c84e64e4a33e54c9fe5918ec
Tones55 <54862709+Tones55@users.noreply.github.com>
2023-03-13 20:04:03 -0400

    inactive message

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,16 @@
 public class Node {
 
+    /*
+     * currently unused, there was an issue when using this object
+     * pointers did not work as expected when using this object inside DefaultMutableTreeNode
+     * instead of usign this object a string is used to represent a token
+     * the string is formatted as follows: "<token.value> <SPACE> <token.position.line> <SPACE> <token.position.column> <SPACE> <token.name>"
+     * an example would be: "int 12 1 keyword" or "7 5 16 digit"
+     * the data is then extracted using String.split and Integer.parseInt
+     * if the issue is resolved this object will be used again
+     */
+
+
     private static String name;
     private static Token tokenPointer;
 
